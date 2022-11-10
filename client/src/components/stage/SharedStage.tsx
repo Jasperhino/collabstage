64188faae396adddb881f141da00375cb6908263
Jasperhino <blum.jasper@gmail.com>
2022-11-10 17:34:19 +0100

    Fix QRcode margin

--- client/src/components/stage/SharedStage.tsx
+++ client/src/components/stage/SharedStage.tsx
@@ -54,18 +54,17 @@ export default function SharedStage() {
         <p>Waiting for fellow actors...</p>
       </div>
       <div className="card lg:card-side bg-base-100 shadow-xl">
-        <div className="m-4">
-          <figure>
-            <a href={'join'} target="_blank">
-              <QRCodeSVG value={`${window.location.host}/stage/join/${stageId}`} size={200} />
-            </a>
-          </figure>
-        </div>
-        <div className="card-body">
-          <h1 className="card-title text-6xl">{state && state.stageId}</h1>
-          <p className="text-left">Scan the QR-Code or enter the code above to join this stage.</p>
+        <div className="flex m-4 flex-row items-center">
+          <a href={'join'} target="_blank" rel="noreferrer">
+            <QRCodeSVG value={`${window.location.host}/stage/join/${stageId}`} size={200} />
+          </a>
+          <div className="card-body">
+            <h1 className="card-title text-6xl">{state && state.stageId}</h1>
+            <p className="text-left">Scan the QR-Code or enter the code above to join this stage.</p>
+          </div>
         </div>
       </div>
+
       <div className="toast toast-top">
         {toasts.map((toast: IToast, i) => (
           <div key={i} className="alert bg-base-100">
