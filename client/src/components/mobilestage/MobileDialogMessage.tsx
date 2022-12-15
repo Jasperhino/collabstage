bef281fa67275ee788b26d9899fc599c4129ca83
Miguel-Soeiro <101994439+Miguel-Soeiro@users.noreply.github.com>
2022-12-15 17:27:33 +0000

    Aligned the teleprompter

--- client/src/components/mobilestage/MobileDialogMessage.tsx
+++ client/src/components/mobilestage/MobileDialogMessage.tsx
@@ -21,58 +21,53 @@ export default function MobileDialogMessage({
   const color = {
     upcoming: 'bg-white',
     current: 'bg-primary',
-    done: 'bg-gray-800',
+    done: 'bg-gray-300',
+  };
+  const gray = {
+    upcoming: 'grayscale(0%)',
+    current: 'grayscale(0%)',
+    done: 'grayscale(60%)',
   };
 
 
 
+
   return (
-    <ul className="menu rounded-box"  style={{ zIndex: '10' }}>
+    <ul className="menu rounded-box">
       <li>
         {!right ?
           <div className= {`flex m-4  place-items-start ${color[state]}`}>
 
             <div className="avatar" >
               <div className="w-12 rounded-full">
-                <img src={avatar} />
+                <img src={avatar}/>
               </div>
             </div>
             <div className="flex flex-col">
               <div className="flex flex-row">
 
-                <span className="text-lg  font-bold opacity-100">{character}</span>
-                <span className="text-lg font-semibold text-red-900 opacity-60 ">({emotion})</span>
-              
+                <span className="text-2lg  font-bold opacity-100">{character}</span>
+                <span className="text-2lg font-semibold text-red-900 opacity-60 ">({emotion})</span>
               </div>
-
-              <span className="text-s opacity-100 ">{text}</span>
-
-
+              <span className="text-2s opacity-100 ">{text}</span>
             </div>
           </div>
 
           :
 
-          <div className= {`flex m-4  place-items-start ${color[state]}`}>
-
+          <div className= {`flex-end m-4  place-items-start ${color[state]}` } style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
             <div className="flex flex-col">
               <div className="flex flex-row">
-
                 <span className="text-lg  font-semibold opacity-100">{character}</span>
                 <span className="text-lg font-bold text-red-900 opacity-60 ">({emotion})</span>
-              
               </div>
-
               <span className="text-s opacity-100 ">{text}</span>
-
-
             </div>
             <div className="avatar" style={{ paddingLeft: "5%" }}>
               <div className="w-24 rounded-full">
                 <img src={avatar} />
               </div>
             </div>
-
           </div>
         }
 
